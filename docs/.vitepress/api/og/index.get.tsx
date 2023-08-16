e3741df368e3c66606b028540b2df6968f1f818b
Eric Fennis <eric.fennis@gmail.com>
2023-08-16 22:50:45 +0200

    add og image

---
+++ docs/.vitepress/api/og/index.get.tsx
@@ -0,0 +1,37 @@
+import { eventHandler, setResponseHeader, defaultContentType, sendWebResponse } from 'h3'
+import { ImageResponse } from '@vercel/og';
+// import OGImage from './OGImage.tsx';
+import React from 'react'
+import { renderToString, renderToStaticMarkup } from 'react-dom/server'
+
+export default eventHandler((event) => {
+  // defaultContentType(event, 'text/html')
+  // defaultContentType(event, 'image/png')
+  // setResponseHeader(event, 'Cache-Control', 'public,max-age=31536000')
+
+  const response = new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 40,
+          color: 'black',
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          padding: '50px 200px',
+          textAlign: 'center',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        👋 Hello world
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    },
+  )
+
+  return sendWebResponse(event, response)
+})
--- docs/nitro.config.ts
+++ docs/nitro.config.ts
@@ -1,20 +1,24 @@
-import { defineNitroConfig } from "nitropack";
-
 export default defineNitroConfig({
   preset: 'vercel-edge',
   srcDir: '.vitepress',
   routeRules: {
     '/api/**': { cors: false },
   },
+  experimental: {
+    wasm: true,
+  },
   esbuild: {
     options: {
-      include: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx', '**/*.json'],
-      loaders: {
-        '.js': 'js',
-        '.jsx': 'jsx',
-        '.ts': 'ts',
-        '.tsx': 'tsx',
-      }
+      // jsx: 'transform',
+      jsxFactory: 'React.createElement',
+      jsxFragment: 'React.Fragment',
+      // include: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx', '**/*.json'],
+      // loaders: {
+      //   '.js': 'js',
+      //   '.jsx': 'jsx',
+      //   '.ts': 'ts',
+      //   '.tsx': 'tsx',
+      // }
     },
   },
 })
