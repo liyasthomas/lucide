08029da2c8ec3726f0342ce0b18c62a52cfb0412
Eric Fennis <eric.fennis@gmail.com>
2023-08-17 17:00:29 +0200

    clean up

--- docs/nitro.config.ts
+++ docs/nitro.config.ts
@@ -1,5 +1,4 @@
 import copy from 'rollup-plugin-copy';
-import { wasm } from '@rollup/plugin-wasm';
 import replace from '@rollup/plugin-replace';
 
 export default defineNitroConfig({
@@ -8,20 +7,13 @@ export default defineNitroConfig({
   routeRules: {
     '/api/**': { cors: false },
   },
-  // experimental: {
-  //   wasm: true,
-  // },
   rollupConfig: {
     external: ['@resvg/resvg-wasm/index_bg.wasm', './resvg.wasm?module'],
     plugins: [
       copy({
         targets: [
-          { src: './node_modules/@vercel/og/dist/yoga.wasm', dest: './.vercel/output/functions/__nitro.func' },
-          { src: './node_modules/@vercel/og/dist/resvg.wasm', dest: './.vercel/output/functions/__nitro.func' }
+          { src: './node_modules/@resvg/resvg-wasm/index_bg.wasm', dest: './.vercel/output/functions/__nitro.func' }
         ]
-      }),
-      wasm({
-        sync: ['@resvg/resvg-wasm/index_bg.wasm']
       }),
       replace({
         include: ['./**/*.ts'],
@@ -33,16 +25,8 @@ export default defineNitroConfig({
   },
   esbuild: {
     options: {
-      // jsx: 'transform',
       jsxFactory: 'React.createElement',
       jsxFragment: 'React.Fragment',
-      // include: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx', '**/*.json'],
-      // loaders: {
-      //   '.js': 'js',
-      //   '.jsx': 'jsx',
-      //   '.ts': 'ts',
-      //   '.tsx': 'tsx',
-      // }
     },
   },
 })
